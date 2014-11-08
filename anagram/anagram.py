042fa9acc6e0607c68bd2c0480a3f84024dcb830
David Sloan <durandal42@gmail.com>
2014-11-07 18:12:03 -0800

    anagram: readability and performance improvements

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,46 +1,31 @@
-from utils import count
-
-names = set()
-for line in open('names.txt'):
-  name = line.strip().upper().split(',')[0]
-  names.add(name)
-  #  print name
-
-
-'''
-Given two maps of element->count, return the difference.
-
-If any value in the result would be negative, instead return None.
-'''
-def subtract_counts(left, right):
-  result = {}
-  for k,v in right.iteritems():
-    if k not in left:
-      return None
-  for k,v in left.iteritems():
-    if k in right:
-      if left[k] > right[k]:
-        result[k] = left[k] - right[k]
-      elif left[k] < right[k]:
-        return None
-    else:
-      result[k] = left[k]
-  return result
-
-# print subtract_counts(count(target),count('MIRIAM'))
+import collections
 
 '''
 Given a string target, find a pair of names which anagram to it.
 '''
-def find_anagram(target):
-  target_counts = count(target)
-  for name1 in names:
-    name1_counts = count(name1)
-    remaining = subtract_counts(target_counts, name1_counts)
-    if not remaining: continue
-    for name2 in names:
-      if count(name2) == remaining:
-        print name1,name2
+def find_anagram(target, names):
+  target_count = collections.Counter(target)
+
+  names_counts = [(n, collections.Counter(n)) for n in names]
+
+  # Filter names that contain letters not in the target:
+  names_counts = filter(lambda nc: target_count | collections.Counter(nc[0]) == target_count,
+                        names_counts)
+
+  for i, (name1, name1_count) in enumerate(names_counts):
+    # Zero and negative counts are dropped when subtracting.
+    # Because we pre-filtered names, counts can't be negative.
+    remaining = target_count - name1_count
+
+
+    for name2, name2_count in names_counts[i:]:
+      if name2_count == remaining:
+        yield name1, name2
+
+assert (list(find_anagram('HEBUGSGORE',
+                          ['GEORGE', 'BUSH', 'RALPH', 'NADER'])) ==
+        [('GEORGE', 'BUSH')])
 
 # someone on OKC said this was an anagram of her first and middle names. :P
-find_anagram("ARMYAIRLINE")
+for n1, n2 in find_anagram('ARMYAIRLINE', [line.strip().upper() for line in open('names.txt')]):
+  print n1, n2
