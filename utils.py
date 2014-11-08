34dc271c1d880f996c8e61fed6aaf66a304adbd5
David Sloan <durandal42@gmail.com>
2014-11-07 18:10:58 -0800

    Remove utils.count
    use collections.Counter instead

--- utils.py
+++ utils.py
@@ -1,18 +1,8 @@
 class Memoize:
-  def __init__(self, f, display=False):
+  def __init__(self, f):
     self.f = f
     self.memo = {}
-    self.display = display
   def __call__(self, *args):
     if not args in self.memo:
       self.memo[args] = self.f(*args)
-      if self.display:
-        print 'memoized%s = %s' % (args, self.memo[args])
     return self.memo[args]
-
-import collections
-def count(word):
-  result = {}
-  for letter in word:
-    result[letter] = result.get(letter, 0) + 1
-  return result
