af629bb2c80cfc71ff295a6256f695f0c2ff4067
David Sloan <durandal42@gmail.com>
2014-11-18 16:09:19 -0800

    [fractals] more comments

--- fractals/source/Constant.java
+++ fractals/source/Constant.java
@@ -1,12 +1,14 @@
+// A constant function: y = c
 public class Constant implements Function {
 
+  // Some useful constants:
   public static final Constant ZERO = new Constant(Complex.ZERO);
   public static final Constant ONE = new Constant(Complex.ONE);
 
-  Complex c;
+  private Complex c;
 
-  public Constant(Complex comp) {
-    c = comp;
+  public Constant(Complex c) {
+    this.c = c;
   }
 
   public Complex apply(Complex input) {
@@ -14,6 +16,7 @@ public class Constant implements Function {
   }
 
   public Function differentiate() {
+    // The derivative of any constant function is zero.
     return ZERO;
   }
 
