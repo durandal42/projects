664cd0aac471c9152e56b8f3a9f0f62547228d67
David Sloan <durandal42@gmail.com>
2015-08-16 12:07:38 -0700

    consider letter frequency in unused tile penalty

--- alphabears/alphabears.py
+++ alphabears/alphabears.py
@@ -6,16 +6,20 @@ from itertools import islice
 
 def parse_available(string):
   result = defaultdict(list)
-  for letter, duration in zip(string[::2], string[1::2]):
-    duration = int(duration)
-    if duration == 0: duration = 10
-    result[letter].append(duration)
+  duration = -1
+  for c in string:
+    if c.isdigit():
+      duration = int(c)
+      if duration == 0: duration = 10
+    else:
+      result[c].append(duration)
   for letter, durations in result.iteritems():
     durations.sort()
   return result
 
 def legal_plays(available, words):
-  available_tiles = Counter(dict((letter, len(durations)) for letter, durations in available.iteritems()))
+  available_tiles = Counter(dict((letter, len(durations))
+                                 for letter, durations in available.iteritems()))
   num_available = sum(available_tiles.values()) 
   print available_tiles
   for word in words:
@@ -23,18 +27,22 @@ def legal_plays(available, words):
     if available_tiles | Counter(word) == available_tiles:
       yield word
 
-def score_play(available, play):
+def score_play(available, play, letter_frequencies):
   play = Counter(play)
   result = 0
   for letter, durations in available.iteritems():
     for unused_tile_duration in durations[play[letter]:]:
-      result -= 10 ** (10 - unused_tile_duration)
+      result -= 2.0 ** -unused_tile_duration  # imminently expiring: -2.0
+      result -= 1000.0 / float(letter_frequencies[letter])
   return result
 
-def best_plays(available, words):
-  return sorted((score_play(available, play), play) for play in legal_plays(available, words))
+def best_plays(available, words, letter_frequencies):
+  return sorted((score_play(available, play, letter_frequencies), play)
+                for play in legal_plays(available, words))
 
 def main(words):
+  letter_frequencies = Counter(c for word in words for c in word)
+  print 'letter_frequencies:', letter_frequencies
   while True:
     print '$ ',
     input = sys.stdin.readline()
@@ -42,7 +50,7 @@ def main(words):
       return
     available_string = input.strip().lower()
     available = parse_available(available_string)
-    for result in best_plays(available, words):
+    for result in best_plays(available, words, letter_frequencies):
       print result
 
 if __name__ == '__main__':
