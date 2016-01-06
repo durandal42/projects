2e876738072c15411276f960aa4628668ca537a9
durandal42 <durandal42@gmail.com>
2016-01-06 11:52:18 -0800

    tests for alphabet deduction

--- alphabet/derive.py
+++ alphabet/derive.py
@@ -6,6 +6,11 @@ def derive_from_pair(left, right):
     if cl != cr: return (cl, cr)
   return None
 
+assert derive_from_pair('a', 'b') == ('a', 'b')
+assert derive_from_pair('abcd', 'abce') == ('d', 'e')
+assert derive_from_pair('', 'b') == None
+assert derive_from_pair('b', 'b') == None
+
 def derive_from_list(words):
   previous = None
   for word in words:
@@ -14,12 +19,16 @@ def derive_from_list(words):
       if r: yield r
     previous = word
 
+assert set(derive_from_list(['', 'a', 'abcd', 'abce', 'b'])) == set([('d','e'), ('a', 'b')])
+
 def build_graph(edges):
   g = collections.defaultdict(set)
   for a,b in edges:
     g[a].add(b)
-    g[b]
+    g[b]  # create empty set of children of b
   return g
+
+assert build_graph([(0,1), (1,2), (2,3), (3,1), (3,4)]) == {0:set([1]), 1:set([2]), 2:set([3]), 3:set([1,4]), 4:set([])}
 
 def topo_sort(graph):
   incoming_count = collections.defaultdict(int)
@@ -28,7 +37,9 @@ def topo_sort(graph):
       incoming_count[dest] += 1
   no_incoming = [n for n in graph.keys() if incoming_count[n] == 0]
   while graph:
-    if not no_incoming: print 'cycle!', graph
+    if not no_incoming:
+      print 'cycle!', graph
+      return
     if len(no_incoming) > 1: print 'ambiguous!', no_incoming
     root = no_incoming.pop()
     for dest in graph[root]:
@@ -36,6 +47,10 @@ def topo_sort(graph):
       if incoming_count[dest] == 0: no_incoming.append(dest)
     del graph[root]
     yield root
+
+assert list(topo_sort({0:set([1]), 1:set([2]), 2:set([3]), 3:set([1,4]), 4:set([])})) == [0]
+assert list(topo_sort({0:set([1]), 1:set([2]), 2:set([3]), 3:set([4]), 4:set([])})) == [0, 1, 2, 3, 4]
+assert list(topo_sort({0:set([1,2,3,4]), 1:set([2,3,4]), 2:set([3,4]), 3:set([4]), 4:set([])})) == [0, 1, 2, 3, 4]
 
 import sys
 words = [word.strip().lower() for word in sys.stdin.readlines()]
