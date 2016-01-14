37f1a9a1cede0b6bef0f2e12dbf7ef744824c269
David Sloan <durandal42@gmail.com>
2016-01-13 16:32:57 -0800

    use key instead of cmp

--- alphabet/sort_by.py
+++ alphabet/sort_by.py
@@ -4,18 +4,9 @@ import collections
 def build_index_map(alpha):
   return collections.defaultdict(int, [(c,i) for i,c in enumerate(alpha)])
 
-def compare_words_by_imap(left, right, imap):
-  for cl, cr in itertools.izip(left, right):
-    diff = imap[cl.lower()] - imap[cr.lower()]
-    if diff: return diff
-  return len(left) - len(right)
-
-def cmp_by_alpha(alpha):
-  imap = build_index_map(alpha)
-  return lambda x, y: compare_words_by_imap(x, y, imap)
-
 def sort_by_alpha(list, alpha):
-  list.sort(cmp=cmp_by_alpha(alpha))
+  imap = build_index_map(alpha.lower())
+  list.sort(key=lambda word: tuple([imap[letter] for letter in word.lower()]))
 
 import sys
 alphabet = len(sys.argv) > 1 and sys.argv[1] or "abcdefghijklmnopqrstuvwxyz"
