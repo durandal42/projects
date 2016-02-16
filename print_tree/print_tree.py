469d1780267bb541da3e8385286523ba8644e26f
David Sloan <durandal42@gmail.com>
2016-02-16 14:07:10 -0800

    print tree in directory style

--- print_tree/print_tree.py
+++ print_tree/print_tree.py
@@ -4,12 +4,45 @@ class Node(object):
     self.left = left
     self.right = right
 
+'''
+States:
+0 - before first child
+1 - seeing first child now
+2 - between first and second child
+3 - seeing second child now
+4 - past second child
 
-def preorder(node):
+To generalize past 2 children, would probably want to collapse into:
+- more children remain
+- seeing child now
+- no more children remain
+'''
+
+def preorder_directory(node, states=[]):
   if not node: return
-  yield node.name
-  for name in preorder(node.left): yield name
-  for name in preorder(node.right): yield name
+  yield None, states
+  if states: states[-1] += 1
+  yield node.name, states
+  if states: states[-1] += 1
+  states += [0]
+  for child in [node.left, node.right]:
+    for n, op in preorder_directory(child, states): yield n, op
+  del states[-1]
+
+def print_directory_tree(node):
+  for name, states in preorder_directory(node):
+    line = ''
+    for op in states:
+      if op in [1, 3]:
+        line += '+-'
+      elif op in [0, 2]:
+        line += '| '
+      elif op == 4:
+        line += '  '
+      else:
+        line += str(op) + " "
+    if name: line += name
+    print line
 
 ROOT = Node('A',
             Node('B',
@@ -21,7 +54,4 @@ ROOT = Node('A',
                       Node('I')),
                  Node('G')))
 
-for name in preorder(ROOT):
-  print name
-
-# TODO(durandal) ascii art
+print_directory_tree(ROOT)
