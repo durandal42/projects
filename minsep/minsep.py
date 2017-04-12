34ef6b7d6dbd4948ecdbc58ec5aa8ffa9481d28a
David Sloan <durandal42@gmail.com>
2017-04-11 20:29:38 -0700

    linear minsep implementation

--- minsep/minsep.py
+++ minsep/minsep.py
@@ -33,6 +33,62 @@ def separate(input, d):
 
   return output
 
+# Fancy O(N) implementation using inverted map.
+def separate_linear(input, d):
+  # Basic map of counts:
+  counts = collections.Counter(input)
+
+  # Inverted map of count:[items]
+  inverted_map = collections.defaultdict(Queue.Queue)
+  for item,count in counts.iteritems():
+    inverted_map[count].put(item)
+
+  # Distinct counts of elements, sorted ascending:
+  distinct_counts = sorted(inverted_map.keys())
+
+  output = []
+  while True:
+    # Pop top k by count from inverted map.
+    batch = []
+    while len(batch) < d and inverted_map:
+      max_count = distinct_counts[-1]  # Max count is always at the end of distinct_counts
+      item = inverted_map[max_count].get(block=False)
+      batch.append(item)
+      counts[item] -= 1
+
+      # We might have exhausted the queue of elements which had this count.
+      if inverted_map[max_count].empty():
+        del inverted_map[max_count]
+        distinct_counts.pop()
+
+    output += batch
+
+    # We might be done:
+    if len(output) == len(input): return output
+    # If we're not done, we might be stuck:
+    if len(batch) < d: return None
+
+    # If we're not done and not stuck, we have more work to do.
+    # Push batched elements back into map, with new counts.
+    new_counts = []
+    for item in batch:
+      if counts[item] == 0: continue
+      if counts[item] not in inverted_map:
+        new_counts.append(counts[item])
+      # Elements returned to their per-count queues in the same order we got them out!
+      inverted_map[counts[item]].put(item)
+
+    # new_counts is now in descending order, and the lowest might be one lower than the highest remaining in distinct_counts.
+    # In other words, distinct_counts + reversed(new_counts) is very nearly sorted, but the middle two elements might be out of order.
+    # ... so tidy up those two elements before appending!
+    if distinct_counts and new_counts and distinct_counts[-1] > new_counts[-1]:
+      distinct_counts[-1], new_counts[-1] = new_counts[-1], distinct_counts[-1]
+    # ... and then append.
+    distinct_counts += reversed(new_counts)
+
+    # All of this bookeeping was to avoid sorting the entire keyset again; check that it worked.
+    # assert distinct_counts == sorted(inverted_map.keys())  # ... only if we're debugging though :)
+
 # "Functional" implementation which never mutates anything.
 # Instead, copy our entire state into the recursive call, and then blow our recursion limit. Wheee!
 def separate_functional(input, d):
@@ -76,7 +132,7 @@ def test(f, input, d, solveable=True):
   else:
     assert output is None
 
-for f in [separate, separate_functional]:
+for f in [separate, separate_linear]:
   # Starter examples:
   test(f, [1,2,2], 1)
   test(f, [1,2,2], 2)
@@ -86,5 +142,7 @@ for f in [separate, separate_functional]:
   test(f, [1,2,2,3,3,3,3], 2)
   test(f, [1,1,2,2,3,3], 2)
 
+  test(f, [1,1,2,2,3,3], 3)
+
   # Tail-recursion test:
-  test(f, [1,2]*1000, 2)
+  #test(f, [1,2]*1000, 2)
