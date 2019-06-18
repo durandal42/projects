8b5c7e19fad566084a96259f14d037c376f578e7
David Sloan <durandal42@gmail.com>
2019-06-17 20:07:08 -0700

    pyformat

--- random_analysis/nyctala.py
+++ random_analysis/nyctala.py
@@ -1,5 +1,6 @@
 from distribution import *
 
+
 def nyctala(sneak=2, dex=3, prof=2,
             advantage=True,
             elven_accuracy=False, archery=False, sharpshooter=False):
@@ -19,6 +20,7 @@ def summarize(d):
 
 summarize(nyctala())
 
+
 def compare_ea_vs_dex(sneak, AC):
   ea_adv = (nyctala(sneak=sneak, dex=3, elven_accuracy=True).ev() -
             nyctala(sneak=sneak, dex=4, elven_accuracy=False).ev())
@@ -33,6 +35,7 @@ def compare_ea_vs_dex(sneak, AC):
     print "[" + " " * int(100 * p) + "*" * (100 - int(100 * p)) + "]"
   print "%dd6 SA vs %02d AC: %s" % (sneak, AC, conclusion)
 
+
 def compare_ss(sneak, AC, elven_accuracy, archery=False):
   ss = nyctala(sneak=sneak, elven_accuracy=elven_accuracy,
                archery=archery, sharpshooter=True).ev()
@@ -40,26 +43,29 @@ def compare_ss(sneak, AC, elven_accuracy, archery=False):
                  archery=archery, sharpshooter=False).ev()
   print "\t%d\t%d\t%s\t%s" % (sneak, AC, ("ss" if ss > noss else "noss"), float(ss - noss))
 
+
 def sneak_dice(level):
-  return (level+1)/2
+  return (level + 1) / 2
+
 
 def likely_ac(level):
   ac_by_level = monsterAC(level)
   return range(ac_by_level - 5, ac_by_level + 6)
+
 
 def circumstances_by_level_range(levels):
   for l in levels:
     for ac in likely_ac(l):
       yield (l, sneak_dice(l), ac)
 
-for level, sneak, AC in circumstances_by_level_range(range(1,13)):
+for level, sneak, AC in circumstances_by_level_range(range(1, 13)):
   compare_ea_vs_dex(sneak, AC)
   # pass
 
 for elven_accuracy in [False, True]:
   print "elven_accuracy: ", elven_accuracy
   print "\tlevel, sneak, AC, judgment, delta"
-  for level, sneak, AC in circumstances_by_level_range(range(1,13)):
+  for level, sneak, AC in circumstances_by_level_range(range(1, 13)):
     print "\t", level,
     compare_ss(sneak, AC, elven_accuracy)
   print
