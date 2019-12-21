805ca000bc1a52e337b45661bb13b81769f9e4aa
durandal42 <durandal42@gmail.com>
2019-12-21 11:53:06 -0800

    make movie.sh more robust

--- maze/movie.sh
+++ maze/movie.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-ffmpeg -r 30 -i images/${1}/%d.png -vcodec h264 -y -pix_fmt yuv420p movies/${1}.mp4 && \
-echo 'cleaning up' images/${1}/ && \
+set -e
+
+mkdir -p movies
+
+echo 'combining images into movie...'
+ffmpeg -r 30 -i images/${1}/%d.png -vcodec h264 -y -pix_fmt yuv420p movies/${1}.mp4
+
+echo 'cleaning up' images/${1}/
 rm -rf images/${1}/
