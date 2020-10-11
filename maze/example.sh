2ac0104b938bce8c6672aa3cc39b74ad682a259c
durandal42 <durandal42@gmail.com>
2020-10-11 13:04:20 -0700

    formatting

--- maze/example.sh
+++ maze/example.sh
@@ -1,1 +1,2 @@
-javac *.java && java Maze -a wilson -c rainbow -r 3072 1920 -f 360 | xargs ./movie.sh && rsync -ra movies/ /Volumes/LaCie\ 6TB/Media/Images/mazes
+javac *.java && java Maze -a wilson -c rainbow -w -r 3072 1920 -f 360 \
+| xargs ./movie.sh && rsync -ra movies/ /Volumes/LaCie\ 6TB/Media/Images/mazes
