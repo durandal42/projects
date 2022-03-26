bc88798369096a8b0213cf872f8166a424d4b761
durandal42 <durandal42@gmail.com>
2022-03-25 21:34:15 -0700

    Support free 1st-level half-feat.

--- random_analysis/pointbuy_roulette.py
+++ random_analysis/pointbuy_roulette.py
@@ -18,41 +18,42 @@ pointbuy_legal = pointbuy.stat_array(
 print("Number of pointbuy-legal stat arrays:", len(pointbuy_legal))
 
 STAT_WEIGHTS_BY_CLASS = {
-    "Artificer": (0, 7, 3, 5, 2, 0),
-    "Barbarian": (6, 2, 3, 0, 2, 0),
-    "Bard": (0, 2, 3, 1, 2, 6),
-    "Bard(Valor)": (0, 7, 3, 1, 2, 6),
-    "Cleric(Heavy)": (1, 1, 3, 1, 7, 1),
-    "Cleric(Medium)": (0, 2, 3, 1, 7, 1),
-    "Druid": (0, 2, 3, 1, 7, 0),
-    "Fighter(DEX EK)": (0, 7, 3, 5, 2, 0),
-    "Fighter(DEX)": (0, 7, 2, 0, 2, 0),
-    "Fighter(STR EK)": (7, 1, 4, 5, 2, 0),
-    "Fighter(STR)": (7, 1, 3, 0, 2, 0),
-    "Monk": (0, 7, 3, 1, 6, 0),
-    "Paladin": (7, 1, 4, 0, 2, 6),
-    "Ranger(DEX)": (0, 7, 3, 1, 2, 1),
-    "Ranger(STR)": (6, 2, 4, 1, 2, 1),
-    "Rogue(AT)": (0, 8, 3, 6, 2, 1),
-    "Rogue(non-AT)": (0, 8, 2, 1, 2, 1),
-    "Sorcerer": (0, 2, 3, 1, 2, 5),
-    "Warlock": (0, 2, 3, 1, 2, 5),
-    "Wizard": (0, 2, 3, 6, 2, 0),
+    "Artificer": (0, 8, 3, 4, 2, 0),
+    "Barbarian": (6, 3, 4, 0, 2, 0),
+    "Bard": (0, 3, 3, 1, 2, 6),
+    "Bard (Valor)": (0, 8, 3, 1, 2, 6),
+    "Cleric (Heavy)": (2, 1, 3, 1, 7, 1),
+    "Cleric (Medium)": (0, 3, 3, 1, 7, 1),
+    "Druid": (0, 3, 3, 1, 7, 0),
+    "Fighter (DEX EK)": (0, 8, 3, 3, 2, 0),
+    "Fighter (DEX)": (0, 8, 2, 0, 2, 0),
+    "Fighter (STR EK)": (8, 1, 4, 3, 2, 0),
+    "Fighter (STR)": (8, 1, 3, 0, 2, 0),
+    "Monk ": (0, 8, 3, 1, 6, 0),
+    "Paladin": (8, 1, 4, 0, 2, 5),
+    "Ranger (DEX)": (0, 8, 3, 1, 5, 1),
+    "Ranger (STR)": (8, 2, 4, 1, 5, 1),
+    "Rogue (AT)": (0, 9, 3, 4, 2, 1),
+    "Rogue (non-AT)": (0, 9, 2, 1, 2, 1),
+    "Sorcerer ": (0, 3, 3, 1, 2, 5),
+    "Warlock": (0, 3, 3, 1, 2, 5),
+    "Wizard": (0, 3, 3, 6, 2, 0),
 }
 
-RACIAL_BONUS_INDICES = distribution.die(6).product(
-    distribution.die(6)).filter(lambda x: x[0] != x[1])
-# print(RACIAL_BONUS_INDICES)
+RACIAL_BONUS_INDICES = distribution.product(
+    [distribution.die(6), distribution.die(6), distribution.die(6)]).filter(lambda x: x[0] != x[1])
+print(RACIAL_BONUS_INDICES)
 
 
 def pick_racials(stats, weights):
   best_stats = None
   best_score = -100
   for bonuses, _ in RACIAL_BONUS_INDICES.items():
-    plus_two, plus_one = bonuses
+    plus_two, plus_one, feat_plus_one = bonuses
     stats_after_racials = list(stats)
     stats_after_racials[plus_two - 1] += 2
     stats_after_racials[plus_one - 1] += 1
+    stats_after_racials[feat_plus_one - 1] += 1
     stats_after_racials = tuple(stats_after_racials)
     score = score_array(stats_after_racials, weights)
     best_score, best_stats = max(
@@ -69,13 +70,18 @@ BUCKET_BOUNDARIES_BY_CLASS = {}
 for c, weights in STAT_WEIGHTS_BY_CLASS.items():
   print()
   print(c)
-  stat_lines = pointbuy_legal.map(lambda x: pick_racials(x, weights))
-  scores = stat_lines.map(lambda x: score_array(x, weights))
-  distribution.summarize(scores)
 
-  print("score distribution:")
-  stats_and_scores = sorted([(score_array(stats, weights), stats)
-                             for stats, p in stat_lines.items()])
+  # print("score distribution:")
+  # distribution.summarize(pointbuy_legal
+  #                        .map(lambda x: pick_racials(x, weights))
+  #                        .map(lambda x: score_array(x, weights)))
+
+  stats_and_scores = []
+  for stats, p in pointbuy_legal.items():
+    post_racials = pick_racials(stats, weights)
+    stats_and_scores.append(
+        (score_array(post_racials, weights), stats, post_racials))
+  stats_and_scores.sort()
 
   print("best statline: ", stats_and_scores[-1])
   print("worst statline:", stats_and_scores[0])
@@ -83,7 +89,7 @@ for c, weights in STAT_WEIGHTS_BY_CLASS.items():
   NUM_BUCKETS = 100
   bucket_boundaries = [stats_and_scores[math.floor(n * p / NUM_BUCKETS)][0]
                        for p in range(NUM_BUCKETS)]
-  print("percentile boundaries:", bucket_boundaries)
+  # print("percentile boundaries:", bucket_boundaries)
   BUCKET_BOUNDARIES_BY_CLASS[c] = bucket_boundaries
 
 
@@ -97,11 +103,12 @@ def assign_percentile(value, bucket_boundaries):
 print("\n16 random pointbuy-legal statlines:")
 for stats in sample(pointbuy_legal, 16):
   print(stats)
-  evaluation = sorted([(assign_percentile(score_array(pick_racials(stats, weights),
-                                                      weights),
-                                          BUCKET_BOUNDARIES_BY_CLASS[c]),
-                        c, pick_racials(stats, weights))
-                       for c, weights in STAT_WEIGHTS_BY_CLASS.items()],
-                      reverse=True)
+  evaluation = []
+  for c, weights in STAT_WEIGHTS_BY_CLASS.items():
+    post_racials = pick_racials(stats, weights)
+    score = score_array(post_racials, weights)
+    evaluation.append((assign_percentile(score, BUCKET_BOUNDARIES_BY_CLASS[c]),
+                       c, post_racials))
+  evaluation.sort(reverse=True)
   for e in evaluation:
     print("\t", e)
