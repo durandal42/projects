b7d2054e6ff7c157f5f9c13a687f466177653866
durandal42 <durandal42@gmail.com>
2022-05-06 14:59:40 -0700

    A more programmatic approach to actually solving redactles.

--- redactle/ngram.py
+++ redactle/ngram.py
@@ -24,7 +24,7 @@ def build_ngrams(text, ns, ng=None):
 
 
 def words_from_line(line):
-  for word in re.sub("[^a-z0-9_']", ' ', line.lower()).split(' '):
+  for word in re.sub("[\W_]", ' ', line.lower()).split(' '):
     if word:
       yield word
 
@@ -63,15 +63,19 @@ def input_loop(ng):
     predict_next_word(ng, past)
 
 
-def main():
+def ngram_analysis():
   files = sorted(filter(lambda f: f.endswith(".txt"),
                         os.listdir('wiki-cache')))
   # print(files)
   ns = [1]
   ng = None
+  seen_in_articles = collections.Counter()
   for i, f in enumerate(files):
     print("%s/%d\t" % (str(i).zfill(len(str(len(files)))), len(files)), end='')
-    ng = build_ngrams(words_from_file('wiki-cache/' + f), ns, ng)
+    words = list(words_from_file('wiki-cache/' + f))
+    ng = build_ngrams(words, ns, ng)
+    for w in set(words):
+      seen_in_articles[w] += 1
   print("Size of ngram corpus:", len(ng))
   for n in ns:
     print(f"Most common ngrams of length {n}:")
@@ -87,6 +91,50 @@ def main():
   # print(ng)
   input_loop(ng)
 
+    
+def main():
+  files = sorted(filter(lambda f: f.endswith(".txt"),
+                        os.listdir('wiki-cache')))#[:10]
+  article_word_counts = {}
+  for i, f in enumerate(files):
+    print("%s/%d\t" % (str(i).zfill(len(str(len(files)))), len(files)), end='')
+    words = list(words_from_file('wiki-cache/' + f))
+    title = " ".join(words[:5])
+    article_word_counts[title] = collections.Counter(words)
+            
+  while len(article_word_counts) > 1:
+    print("There are %d remaining articles." % len(article_word_counts))
+    if len(article_word_counts) < 10:
+      for title in article_word_counts:
+        print(f"\t{title}")
+    present = collections.Counter()
+    absent = collections.Counter()
+    words = set()
+    for title, word_counts in article_word_counts.items():
+      for word, count in word_counts.items():
+        if word in COMMON_WORDS: continue
+        if count >= 15:
+          present[word] += 1
+          words.add(word)
+        if count <= 5:
+          absent[word] += 1
+          words.add(word)
 
+    decisive_words = sorted(
+        words, key=lambda w: -1 * present.get(w, 0) * absent.get(w, 0))
+    for w in decisive_words[:10]:
+      print("%d,%d: %s" % (present[w], absent[w], w))
+    dw = input("Select a word, perhaps from the suggestions above: ")
+    appears = {'y':True, 'n':False}[input(f"Does {dw} appear 10+ times in the article? [y/n] ")]
+
+    article_word_counts = dict(filter(lambda e: (e[1][dw] >= 10) == appears,
+                                      article_word_counts.items()))
+
+  print("Remaining articles: ")
+  for title, word_counts in article_word_counts.items():
+    print(title)
+    for word, count in collections.Counter(word_counts).most_common(50):
+      print(f"\t{count}:\t{word}")
+        
 if __name__ == '__main__':
   main()
