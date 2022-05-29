628ccbc023890d285aae5511fbdcd4928c628086
durandal42 <durandal42@gmail.com>
2022-05-28 17:41:01 -0700

    Smarter refinement of article list

--- redactle/ngram.py
+++ redactle/ngram.py
@@ -5,9 +5,15 @@ import os
 import functools
 import multiprocessing
 import heapq
-
+import pickle
+import unicodedata
+from statsmodels.stats.weightstats import DescrStatsW
 
 COMMON_WORDS = set(['a', 'aboard', 'about', 'above', 'across', 'after', 'against', 'along', 'amid', 'among', 'an', 'and', 'around', 'as', 'at', 'because', 'before', 'behind', 'below', 'beneath', 'beside', 'between', 'beyond', 'but', 'by', 'concerning', 'considering', 'despite', 'down', 'during', 'except', 'following', 'for', 'from', 'if', 'in', 'inside', 'into', 'is', 'it', 'like', 'minus', 'near', 'next', 'of', 'off', 'on', 'onto', 'opposite', 'or', 'out', 'outside', 'over', 'past', 'per', 'plus', 'regarding', 'round', 'save', 'since', 'than', 'the', 'through', 'till', 'to', 'toward', 'under', 'underneath', 'unlike', 'until', 'up', 'upon', 'versus', 'via', 'was', 'with', 'within', 'without'])
+
+def normalize(s):
+  return unicodedata.normalize('NFKD', s).encode('ascii', 'ignore').decode()
+
 
 def build_ngrams(text, ns, ng=None):
   if ng is None:
@@ -26,16 +32,26 @@ def build_ngrams(text, ns, ng=None):
 def words_from_line(line):
   for word in re.sub("[\W_]", ' ', line.lower()).split(' '):
     if word:
-      yield word
+      for w in re.split(r"(\D+)", word):
+        if w:
+          yield w
 
 
 def words_from_file(filename):
   print("Loading text from file:", filename)
   for line in open(filename):
+    line = normalize(line.strip())
+    if "== Further reading ==" in line: break
+    if "=== External links ===" in line: break
+    if line.startswith("{") and line.endswith("}"): continue
+    if len(line) < 3: continue
     for word in words_from_line(line):
       yield word
 
+#for w in words_from_file('wiki-cache/Foam.txt'):
+#  print(w, end=' ')
 
+      
 def get_past_from_user():
   input_string = input(f'past words: ')
   past = tuple(words_from_line(input_string))
@@ -96,38 +112,53 @@ def main():
   files = sorted(filter(lambda f: f.endswith(".txt"),
                         os.listdir('wiki-cache')))#[:10]
   article_word_counts = {}
-  for i, f in enumerate(files):
-    print("%s/%d\t" % (str(i).zfill(len(str(len(files)))), len(files)), end='')
-    words = list(words_from_file('wiki-cache/' + f))
-    title = " ".join(words[:5])
-    article_word_counts[title] = collections.Counter(words)
-            
+  awc_pickle_file = "article-word-counts.pickle"
+  if os.path.exists(awc_pickle_file):
+    print("Loading pickled word counts...")
+    article_word_counts = pickle.load(open(awc_pickle_file, "rb"))
+  else:
+    for i, f in enumerate(files):
+      # if i > 10: break
+      print("%s/%d\t" % (str(i).zfill(len(str(len(files)))), len(files)), end='')
+      words = list(words_from_file('wiki-cache/' + f))
+      title = " ".join(words[:5])
+      article_word_counts[title] = collections.Counter(words)
+    print("Pickling word counts...")
+    pickle.dump(article_word_counts, open(awc_pickle_file, "wb"))
+
+  # watchword = 'foam'
+    
+  THRESHOLD = 10
+  MARGIN = 5
   while len(article_word_counts) > 1:
-    print("There are %d remaining articles." % len(article_word_counts))
-    if len(article_word_counts) < 10:
+    print("Remaining articles:", len(article_word_counts))
+    if len(article_word_counts) < 30:
       for title in article_word_counts:
         print(f"\t{title}")
-    present = collections.Counter()
-    absent = collections.Counter()
+    present = collections.defaultdict(collections.Counter)
     words = set()
     for title, word_counts in article_word_counts.items():
+      #      if watchword in title:
+      #        print(f'"{watchword}" is in still-possible title "{title}"')
+      #        print("Word counts:", word_counts)
       for word, count in word_counts.items():
         if word in COMMON_WORDS: continue
-        if count >= 15:
-          present[word] += 1
-          words.add(word)
-        if count <= 5:
-          absent[word] += 1
-          words.add(word)
+        present[word][count] += 1
+        words.add(word)
+    print("Number of words appearing in remaining articles:", len(words))
+        
+    buckets = {}
+    for word in words:
+      buckets[word] = len(present[word]) + 1
+        
+    decisive_words = heapq.nlargest(10, words, key=lambda w: buckets[w])
+    for w in decisive_words:
+      print("%d: %s" % (buckets[w], w))
+    dw = input("Select a word, perhaps from the suggestions above: ").strip()
+    appears = int(input(f"How many times does {dw} appear in the article? "))
 
-    decisive_words = sorted(
-        words, key=lambda w: -1 * present.get(w, 0) * absent.get(w, 0))
-    for w in decisive_words[:10]:
-      print("%d,%d: %s" % (present[w], absent[w], w))
-    dw = input("Select a word, perhaps from the suggestions above: ")
-    appears = {'y':True, 'n':False}[input(f"Does {dw} appear 10+ times in the article? [y/n] ")]
-
-    article_word_counts = dict(filter(lambda e: (e[1][dw] >= 10) == appears,
+    threshold = max(1, appears/5)
+    article_word_counts = dict(filter(lambda e: abs(e[1][dw] - appears) < threshold,
                                       article_word_counts.items()))
 
   print("Remaining articles: ")
