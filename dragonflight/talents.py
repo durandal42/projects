ec88b1007fe1e27a9e11aa26fff83dbde4aec2c6
durandal42 <durandal42@gmail.com>
2022-10-31 00:04:23 -0700

    singularize talent trees
    this will make it easier to express talent loadouts as a bitset

--- dragonflight/talents.py
+++ dragonflight/talents.py
@@ -10,39 +10,61 @@ def get_talents_by_name(talents):
 TALENTS = talent_data.get_talents("Paladin")
 
 print("Loaded %d talents:" % len(TALENTS), TALENTS)
-print(get_talents_by_name(TALENTS))
+# print(get_talents_by_name(TALENTS))
 
-def singularize_talent_name(name, i, max):
-  return name + " (%d of %d)" % (i+1, t.points)
+def singularize_talent_name(name, i, max_points):
+  return name + " (%d of %d)" % (i+1, max_points)
 
-# def singularize_talent_tree(talents):
-#   talents_by_name = get_talents_by_name(TALENTS)
-#   parents = collections.defaultdict(list)
-#   children = collections.defaultdict(list)
-#   for t1 in talents:
-#     for t2_name in t1.parents:
-#       parents[t1.name].append(t2_name)
-#       children[t2_name].append(t1.name)
+def singularize_talent_tree(talents):
+  talents_by_name = get_talents_by_name(TALENTS)
+  parents = collections.defaultdict(list)
+  children = collections.defaultdict(list)
+  for t1 in talents:
+    for t2_name in t1.parents:
+      parents[t1.name].append(t2_name)
+      children[t2_name].append(t1.name)
 
-#   parent_corrections = {}
-#   for t in talents:
-#     if t.points > 1:
-#       chain = []
-#       parent_corrections[t.name] = singularize_talent_name(t.name, t.points-1, t.points)
-#       for i in range(t.points):
-#         name = singularize_talent_name(t.name, i, t.points)
-#         if i = 0:
-#           parents = t.parents
-#         else: 
-#           parents = [singularize_talent_name(t.name, i-1, t.points)]
-#         chain.append(Talent(name=name, points=1, row=t.row, col=t.col, parents=parents)
+  parent_corrections = {}
+  talent_names_to_remove = set()
+  talents_to_add = []
+  for t in talents:
+    if t.points > 1:
+      talent_names_to_remove.add(t.name)
+      parent_corrections[t.name] = singularize_talent_name(t.name, t.points-1, t.points)
+      for i in range(t.points):
+        name = singularize_talent_name(t.name, i, t.points)
+        if i == 0:
+          parents = t.parents
+        else: 
+          parents = [singularize_talent_name(t.name, i-1, t.points)]
+        talents_to_add.append(talent_data.Talent(name=name, points=1, row=t.row+i/10, col=t.col, parents=parents))
+
+  talents += talents_to_add
+
+  new_talents = []
+  for t in talents:
+    if t.name in talent_names_to_remove: continue
+    new_parents = []
+    for p in t.parents:
+      if p in parent_corrections:
+        new_parents.append(parent_corrections[p])
+      else:
+        new_parents.append(p)
+    new_talents.append(talent_data.Talent(name=t.name, points=t.points, row=t.row, col=t.col, parents=new_parents))
+
+  return new_talents
+
+TALENTS = singularize_talent_tree(TALENTS)
 
 def data_validate_talents(talents):
   talents_by_name = get_talents_by_name(talents)
   for t1 in talents:
+    if t1.points > 1:
+      print("Didn't singularize talent:", t1)
+      return False
     for t2_name in t1.parents:
       t2 = talents_by_name[t2_name]
-      if not data_validate_dependency(t1, t2): return false
+      if not data_validate_dependency(t1, t2): return False
   return True
 
 # Determine whether a dependency of t1 on t2 is valid.
@@ -62,8 +84,8 @@ LOADOUT = [
   "Auras of the Resolute",
   "Auras of Swift Vengeance",
   "Divine Steed",
-  "Fist of Justice",
-  "Fist of Justice",
+  "Fist of Justice (1 of 2)",
+  "Fist of Justice (2 of 2)",
   "Cavalier",
   "Greater Judgment",
 ]
@@ -74,22 +96,21 @@ FREE_TALENTS = [
 ]
 
 def validate_talent_loadout(talents, free_talents, loadout):
+  if len(loadout) != len(set(loadout)):
+    # TODO(dsloan): handle taking the same talent twice more cleanly
+    return False
   talents_by_name = get_talents_by_name(talents)
-  talent_counts = collections.Counter(loadout)
+  loadout = set(loadout)
   points_spent_by_tier = collections.defaultdict(int)
 
   for t1_name in loadout:
     t1 = talents_by_name[t1_name]
 
-    if talent_counts[t1_name] > t1.points:
-      # print("Cannot spend more than %d points on talent: %s" % (t1.points, t1_name))
-      return False
-
     # check that dependencies are satisfied:
     dependency_satisfied = False
     for t2_name in t1.parents:
       t2 = talents_by_name[t2_name]
-      if talent_counts[t2_name] == t2.points:
+      if t2_name in loadout:
         dependency_satisfied = True
     if len(t1.parents) > 0 and not dependency_satisfied:
       # print("Talent selected without fully selecting at least one dependency:", t1_name)
@@ -110,6 +131,8 @@ def validate_talent_loadout(talents, free_talents, loadout):
     # print("Must spend 20 points on tier1+tier2 talents to unlock tier3 talents.")
     return False
 
+  # TODO(dsloan): check that total number of talent points spent is within budget
+
   return True
 
 assert validate_talent_loadout(TALENTS, FREE_TALENTS, LOADOUT)
@@ -126,7 +149,7 @@ def valid_builds(talents, free_talents, points_to_spend, partial_loadout=None):
 
   if partial_loadout == None: partial_loadout = free_talents
 
-  partial_loadouts = [tuple(partial_loadout)]
+  partial_loadouts = [tuple(sorted(partial_loadout))]
 
   while points_to_spend > 0:
     print("With %d points remaining, %d valid builds so far..." % (points_to_spend, len(partial_loadouts)))
