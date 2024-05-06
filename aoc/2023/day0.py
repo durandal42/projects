26bb527d5135e26b943d34ed9da0b5630dd06957
durandal42 <durandal42@gmail.com>
2024-05-05 23:30:54 -0700

    AoC 2023 template

---
+++ aoc/2023/day0.py
@@ -0,0 +1,18 @@
+from common import assertEqual
+from common import submit
+
+
+def day0(input):
+  pass
+
+test_input = '''
+'''
+test_output = None
+
+assertEqual(test_output, day0(test_input))
+
+
+print('day0 answer:')
+submit(day0(open('day0_input.txt', 'r').read()),
+       expected=None)
+print()
