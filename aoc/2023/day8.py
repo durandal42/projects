8050ad308bb7631974eebcd1a8feffbc1a0bd1f6
durandal42 <durandal42@gmail.com>
2024-05-10 23:23:37 -0700

    AoC 2023 Day 8 Part 2

--- aoc/2023/day8.py
+++ aoc/2023/day8.py
@@ -1,9 +1,15 @@
 from common import assertEqual
 from common import submit
 import re
+import math
 
 
 def day8(input):
+  instructions, nodes = parse_input(input)
+  return follow_instructions('AAA', lambda l: l == 'ZZZ', nodes, instructions)
+
+
+def parse_input(input):
   lines = input.splitlines()
   instructions = lines[0]
   _ = lines[1]
@@ -11,21 +17,22 @@ def day8(input):
   for line in lines[2:]:
     k, v = parse_node(line)
     nodes[k] = v
-  print(nodes)
 
-  location = 'AAA'
+  return instructions, nodes
+
+
+def follow_instructions(src, dst_criteria, nodes, instructions):
+  location = src
   steps = 0
-  while location != 'ZZZ':
+  while not dst_criteria(location):
     location = nodes[location][instructions[steps % len(instructions)]]
     steps += 1
   return steps
 
 
 def parse_node(line):
-  m = re.match('(\w+) = \((\w+), (\w+)\)', line)
-  assert m
+  m = re.match('(\\w+) = \\((\\w+), (\\w+)\\)', line)
   label, left, right = m.group(1), m.group(2), m.group(3)
-  print((label, {'L': left, 'R': right}))
   return (label, {'L': left, 'R': right})
 
 
@@ -57,5 +64,36 @@ assertEqual(test_output, day8(test_input))
 
 print('day8 answer:')
 submit(day8(open('day8_input.txt', 'r').read()),
-       expected=None)
+       expected=12169)
+print()
+
+# part 2 complication
+test_input = '''LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+'''
+test_output = 6
+
+
+def day8(input):
+  instructions, nodes = parse_input(input)
+  srcs = [k for k in nodes.keys() if k.endswith('A')]
+
+  steps = [follow_instructions(src, lambda l: l.endswith('Z'),
+                               nodes, instructions) for src in srcs]
+  print(steps)
+  return math.lcm(*steps)
+
+assertEqual(test_output, day8(test_input))
+
+print('day8, part2 answer:')
+submit(day8(open('day8_input.txt', 'r').read()),
+       expected=12030780859469)
 print()
