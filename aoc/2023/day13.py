eb094f83e7da27b78ac608c848fd55e9f5b245eb
durandal42 <durandal42@gmail.com>
2024-05-25 22:38:09 -0700

    AoC 2023 Day 13 Part 2

--- aoc/2023/day13.py
+++ aoc/2023/day13.py
@@ -2,13 +2,13 @@ from common import assertEqual
 from common import submit
 
 
-def day13(input):
-  return sum(score(pattern.splitlines()) for pattern in input.split('\n\n'))
+def day13(input, error_target=0):
+  return sum(score(pattern.splitlines(), error_target) for pattern in input.split('\n\n'))
 
 
-def score(pattern):
-  h_mirror = find_mirror(pattern)
-  v_mirror = find_mirror(transpose(pattern))
+def score(pattern, error_target):
+  h_mirror = find_mirror(pattern, error_target)
+  v_mirror = find_mirror(transpose(pattern), error_target)
   return (v_mirror or 0) + (h_mirror or 0) * 100
 
 
@@ -22,15 +22,26 @@ assertEqual(['012'], transpose(['0', '1', '2']))
 assertEqual(['12', '34'], transpose(transpose(['12', '34'])))
 
 
-def find_mirror(pattern):
+def mismatches(left, right):
+  assert len(left) == len(right)
+  result = 0
+  for l, r in zip(left, right):
+    if l != r:
+      result += 1
+  return result
+
+
+def find_mirror(pattern, error_target):
   # print(f'looking for mirror in:\n{"\n".join("".join(p) for p in pattern)}')
   for r in range(1, len(pattern)):
     mirror = True
+    mm = 0
     for above, below in zip(reversed(pattern[0:r]), pattern[r:]):
-      if above != below:
+      mm += mismatches(above, below)
+      if mm > error_target:
         mirror = False
         break
-    if mirror:
+    if mirror and mm == error_target:
       return r
 
 
@@ -57,4 +68,32 @@ assertEqual(test_output, day13(test_input))
 print('day13 answer:')
 submit(day13(open('day13_input.txt', 'r').read()),
        expected=35521)
+print()
+
+# part2 complication
+
+
+test_input = '''#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#
+'''
+test_output = 400
+
+assertEqual(test_output, day13(test_input, error_target=1))
+
+print('day13, part2 answer:')
+submit(day13(open('day13_input.txt', 'r').read(), error_target=1),
+       expected=None)
 print()
