4555e7bb849957bedc71d8f9b5e5816708205ec9
durandal42 <durandal42@gmail.com>
2024-12-18 22:28:12 -0800

    stubs

---
+++ aoc/2024/common.py
@@ -0,0 +1,16 @@
+def assertEqual(expected, actual):
+  assert expected == actual, f"expected: {expected}\nactual: {actual}"
+
+
+assertEqual(0, 0)
+try:
+  assertEqual(1, 0)
+  assert False
+except AssertionError:
+  pass
+
+
+def submit(answer, expected=None):
+  if expected is not None:
+    assertEqual(expected, answer)
+  print(answer)
---
+++ aoc/2024/day00.py
@@ -0,0 +1,19 @@
+from common import assertEqual
+from common import submit
+
+
+def day00(input):
+  pass
+
+
+test_input = '''\
+'''
+test_output = None
+
+assertEqual(test_output, day00(test_input))
+
+
+print('day00 answer:')
+submit(day00(open('day00_input.txt', 'r').read()),
+       expected=None)
+print()
