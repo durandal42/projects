12904e1a81625849c8a1b2c7413dc48491d181e5
durandal42 <durandal42@gmail.com>
2024-12-20 15:51:07 -0800

    day 8 part 2

--- aoc/2024/day08.py
+++ aoc/2024/day08.py
@@ -5,6 +5,28 @@ import collections
 import itertools
 
 
+def tuple_add(t1, t2):
+  return tuple(x1 + x2 for x1, x2 in zip(t1, t2))
+
+
+def tuple_scale(t, scale):
+  return tuple(map(lambda x: scale * x, t))
+
+
+def in_range(t, limit_t):
+  return all(c in range(limit_c) for c, limit_c in zip(t, limit_t))
+
+
+def antinodes_from_antennae(loc1, loc2, num_rows, num_cols):
+  delta = tuple_add(loc2, tuple_scale(loc1, -1))
+  a1 = tuple_add(loc1, tuple_scale(delta, -1))
+  a2 = tuple_add(loc2, delta)
+  if in_range(a1, (num_rows, num_cols)):
+    yield a1
+  if in_range(a2, (num_rows, num_cols)):
+    yield a2
+
+
 def day08(input):
   grid = input.splitlines()
   num_rows = len(grid)
@@ -23,13 +45,8 @@ def day08(input):
   antinodes = set()
   for freq, locs in ants_by_freq.items():
     for loc1, loc2 in itertools.combinations(locs, 2):
-      delta = loc2[0] - loc1[0], loc2[1] - loc1[1]
-      a1 = loc1[0] - delta[0], loc1[1] - delta[1]
-      a2 = loc2[0] + delta[0], loc2[1] + delta[1]
-      if a1[0] in range(num_rows) and a1[1] in range(num_cols):
-        antinodes.add(a1)
-      if a2[0] in range(num_rows) and a2[1] in range(num_cols):
-        antinodes.add(a2)
+      for a in antinodes_from_antennae(loc1, loc2, num_rows, num_cols):
+        antinodes.add(a)
 
   return len(antinodes)
 
@@ -56,4 +73,43 @@ assertEqual(test_output, day08(test_input))
 print('day08 answer:')
 submit(day08(open('day08_input.txt', 'r').read()),
        expected=269)
+print()
+
+# part 2 complication
+
+test_output = 34
+
+
+def gcd(a, b):
+  while b > 0:
+    a, b = b, a % b
+  return a
+
+
+def antinodes_from_antennae(loc1, loc2, num_rows, num_cols):
+  delta = tuple_add(loc2, tuple_scale(loc1, -1))
+  if delta[0] == 0:
+    delta = (1, 0)
+  elif delta[1] == 0:
+    delta = (0, 1)
+  else:
+    delta = tuple_scale(delta, 1/gcd(abs(delta[0]), abs(delta[1])))
+
+  t = loc1
+  while in_range(t, (num_rows, num_cols)):
+    yield t
+    t = tuple_add(t, tuple_scale(delta, -1))
+
+  t = tuple_add(loc1, delta)
+  while in_range(t, (num_rows, num_cols)):
+    yield t
+    t = tuple_add(t, delta)
+
+
+assertEqual(test_output, day08(test_input))
+
+
+print('day08, part 2 answer:')
+submit(day08(open('day08_input.txt', 'r').read()),
+       expected=949)
 print()
