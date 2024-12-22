3242e2826b2e76fde21d6f9ec6c7a2af11fcb90c
durandal42 <durandal42@gmail.com>
2024-12-21 21:47:02 -0800

    day 10 part 2

--- aoc/2024/day10.py
+++ aoc/2024/day10.py
@@ -1,6 +1,8 @@
 from common import assertEqual
 from common import submit
 
+import collections
+
 
 def day10(input):
   grid = [[int(s) for s in row] for row in input.splitlines()]
@@ -12,23 +14,28 @@ def day10(input):
 
 
 def score_trailhead(grid, r, c):
-  if grid[r][c] != 0:
-    return 0
+  return len(find_reachable_peaks(grid, r, c))
 
-  reachable = [(r, c)]
+
+def find_reachable_peaks(grid, r, c):
+  if grid[r][c] != 0:
+    return {}
+
+  reachable = {(r, c): 1}
   height = 0
   while height < 9:
-    newly_reachable = set()
+    newly_reachable = collections.defaultdict(int)
     height += 1
-    for r, c in reachable:
+    for rc, num_paths in reachable.items():
+      r, c = rc
       for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
         if (r+dr in range(len(grid)) and
             c+dc in range(len(grid[r+dr])) and
                 grid[r+dr][c+dc] == height):
-          newly_reachable.add((r+dr, c+dc))
+          newly_reachable[(r+dr, c+dc)] += num_paths
     reachable = newly_reachable
 
-  return len(reachable)
+  return reachable
 
 
 test_input = '''\
@@ -47,6 +54,22 @@ assertEqual(test_output, day10(test_input))
 
 
 print('day10 answer:')
+submit(day10(open('day10_input.txt', 'r').read()),
+       expected=574)
+print()
+
+# part 2 complication
+
+test_output = 81
+
+
+def score_trailhead(grid, r, c):
+  return sum(find_reachable_peaks(grid, r, c).values())
+
+
+assertEqual(test_output, day10(test_input))
+
+print('day10, part 2 answer:')
 submit(day10(open('day10_input.txt', 'r').read()),
        expected=None)
 print()
