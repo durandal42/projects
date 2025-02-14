833daec32f4fd316008c45e0e263d9d9cc1b650f
durandal42 <durandal42@gmail.com>
2025-02-13 19:01:32 -0500

    aoc 2024 day 14 part 2 (manual)

--- aoc/2024/day14.py
+++ aoc/2024/day14.py
@@ -4,6 +4,7 @@ from common import sign
 import re
 import collections
 import math
+import time
 
 
 def parse_input(input):
@@ -26,23 +27,24 @@ def extrapolate(pos, v, steps, limit):
 assertEqual((1, 3), extrapolate((2, 4), (2, -3), 5, (11, 7)))
 
 
-def day14(input, x_limit=101, y_limit=103, steps=100):
+def day14(input, limit=(101, 103), steps=100):
   robots = parse_input(input)
-  print(robots)
-
-  limit = (x_limit, y_limit)
+  # print(robots)
 
   final_positions = [extrapolate(pos, v, steps, limit) for pos, v in robots]
-  print(final_positions)
+  final_positions_hypothetical = [extrapolate(pos, v, steps + math.lcm(limit[0], limit[1]), limit) for pos, v in robots]
+  assertEqual(final_positions, final_positions_hypothetical)
+
+  # print(final_positions)
 
   quadrants = collections.Counter()
   for x, y in final_positions:
-    qx = sign(x - x_limit // 2)
-    qy = sign(y - y_limit // 2)
+    qx = sign(x - limit[0] // 2)
+    qy = sign(y - limit[1] // 2)
     if not qx or not qy:
       continue
     quadrants[(qx, qy)] += 1
-  print(quadrants)
+  # print(quadrants)
 
   return math.prod(quadrants.values())
 
@@ -63,10 +65,40 @@ p=9,5 v=-3,-3
 '''
 test_output = 12
 
-assertEqual(test_output, day14(test_input, 11, 7, 100))
+assertEqual(test_output, day14(test_input, (11, 7), 100))
 
 
 print('day14 answer:')
 submit(day14(open('day14_input.txt', 'r').read()),
        expected=230435667)
+print()
+
+# part 2 complications
+
+
+def render(positions, limit):
+  limit_x, limit_y = limit
+  positions = set(positions)
+  print("\n".join("".join((x, y) in positions and "X" or " "
+                          for x in range(limit_x))
+                  for y in range(limit_y)))
+
+
+def day14(input, limit=(101, 103)):
+  robots = parse_input(input)
+
+  for steps in range(math.prod(limit)):
+    final_positions = [extrapolate(pos, v, steps, limit) for pos, v in robots]
+    if steps % 101 != 33:
+      # visually detected significant vertical alignment at step 33
+      continue
+    print("steps:", steps)
+    render(final_positions, limit)
+    time.sleep(1)
+    # return steps
+
+
+print('day14 answer:')
+submit(day14(open('day14_input.txt', 'r').read()),
+       expected=7709)
 print()
