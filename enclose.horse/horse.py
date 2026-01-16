a707823046bc8b704b0eaf22f3c25780e08bbd20
durandal42 <durandal42@gmail.com>
2026-01-15 20:49:29 -0800

    multiprocessing

--- enclose.horse/horse.py
+++ enclose.horse/horse.py
@@ -1,7 +1,9 @@
 import json
 from collections import deque
 from collections import defaultdict
+import queue
 from fractions import Fraction
+import multiprocessing
 import math
 
 
@@ -75,49 +77,116 @@ def print_grid(grid, walls):
   return "".join(result)
 
 
-MAX_FANOUT = defaultdict(int)
-MOST_SPACE = 0
-PROGRESS = 0
+# provided by the master:
+global_grid = None
+global_horse = None
+global_result_q = None
+
+# managed by each worker
+global_work_q = deque()
+global_most_space = 0
 
 
-def solve(grid, horse, budget, walls=set(), notwalls=set(), fanout=1):
-  global MOST_SPACE
-  global PROGRESS
+def init_worker(grid, horse, result_q):
+  global global_grid
+  global_grid = grid
+  global global_horse
+  global_horse = horse
+  global global_result_q
+  global_result_q = result_q
 
-  # print(f"solve(<grid>, {horse}, {budget}, walls={walls}, notwalls={notwalls})")
-  escape_path, space = search(grid, horse, walls)
+
+NUM_WORKERS = 10
+
+
+def solve(grid, horse, budget):
+  print(f"solve({grid}, {horse}, {budget})")
+  m = multiprocessing.Manager()
+  result_q = m.Queue()
+  init_worker(grid, horse, result_q)
+
+  global_work_q.append((budget, set(), set(), 1))
+
+  while global_work_q and len(global_work_q) < NUM_WORKERS:
+    print("fanning out more work units...")
+    budget, walls, notwalls, fanout = global_work_q.popleft()
+    solve_work_unit(budget, walls, notwalls, fanout)
+  print(f"fanned out to {len(global_work_q)} work units.")
+
+  print(f"starting {NUM_WORKERS} workers...")
+  pool = multiprocessing.Pool(
+      processes=NUM_WORKERS, initializer=init_worker, initargs=(grid, horse, result_q,))
+  workers = [pool.apply_async(solve_worker, (work,))
+             for work in global_work_q]
+
+  # Wait for all workers to finish.
+  work_done = False
+  while not work_done:
+    work_done = True
+    for w in workers:
+      process_result_q(result_q)
+      try:
+        w.get(1)
+      except multiprocessing.context.TimeoutError as e:
+        work_done = False
+        pass
+
+  pool.close()
+
+  return global_most_space
+
+
+def process_result_q(result_q):
+  global global_most_space
+  while not result_q.empty():
+    space, solution = result_q.get()
+    if space > global_most_space:
+      print(solution)
+      global_most_space = space
+
+
+def solve_worker(work_root):
+  print(f"solve_worker({work_root})")
+  global global_work_q
+  global_work_q = deque()
+  global_work_q.append(work_root)
+  print("global_work_q:", global_work_q)
+  while global_work_q:
+    work = global_work_q.pop()
+    budget, walls, notwalls, fanout = work
+    solve_work_unit(budget, walls, notwalls, fanout)
+
+
+def solve_work_unit(budget, walls, notwalls, fanout):
+  # print(f"solve_work_unit({budget}, {walls}, {notwalls}, {fanout})")
+  escape_path, space = search(global_grid, global_horse, walls)
 
   if not escape_path:
     # the horse didn't escape, so score this as a possible solution
-    if space > MOST_SPACE:
-      print(f"found a solution using {len(walls)} walls and {space} space!")
-      print(print_grid(grid, walls))
-      print("progress:", PROGRESS, "=", float(PROGRESS))
-      MOST_SPACE = space
-    PROGRESS += Fraction(1, fanout)
-    return space, None
+    global global_most_space
+    if space > global_most_space:
+      global_most_space = space
+      result = (space, f"found a solution using {len(walls)} walls and {space} space!"
+                f"\n{print_grid(global_grid, walls)}")
+      global_result_q.put(result)
+    return
 
   # the horse did escape, so we MUST put a wall somewhere on its escape path
 
   if budget == 0:
     # ... but we're out of budget, so we're done with this branch :(
-    PROGRESS += Fraction(1, fanout)
-    return 0, []
+    return
 
   # iterate over all options for which cell on that path is the FIRST wall
   #   (and therefore everything up to that point is a non-wall,
   #    never to be picked again in this hypothetical branch)
-  options = [i for i in range(len(escape_path) - 1)  # (don't wall the horse itself)
+  options = [i for i in range(len(escape_path) - 1)  # (don't wall the horse)
              if escape_path[i] not in notwalls]
-  if (len(options) > MAX_FANOUT[budget]):
-    print("*"*(10-budget), f"budget: {budget}, fanout: {len(options)}")
-    MAX_FANOUT[budget] = len(options)
-  result = max(solve(grid, horse, budget-1,
-                     walls=walls | set([escape_path[i]]),
-                     notwalls=notwalls | set(escape_path[: i]),
-                     fanout=fanout*len(options))
-               for i in options)
-  return result
+  for i in options:
+    global_work_q.append((budget-1,
+                          walls | set([escape_path[i]]),
+                          notwalls | set(escape_path[:i]),
+                          fanout*len(options)))
 
 
 def main():
@@ -133,4 +202,6 @@ def main():
   print(solve(grid, horse, budget))
 
 
-main()
+if __name__ == '__main__':
+  multiprocessing.freeze_support()
+  main()
